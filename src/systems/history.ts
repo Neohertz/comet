0241a14757bacaf7f5bf527e29266572da1db190
Neohertz <ryan@neohertz.dev>
2025-03-27 17:52:34 -0600

    feat: add try() method to History module

--- src/systems/history.ts
+++ src/systems/history.ts
@@ -3,6 +3,12 @@ import { Log } from "../util/log";
 
 const ChangeHistoryService = game.GetService("ChangeHistoryService");
 
+interface RecordConfig {
+	name: string;
+	description?: string;
+	onSuccess?: "commit" | "append" | "cancel";
+}
+
 /**
  * A system that contains utilities for managing undo/redo waypoints.
  *
@@ -13,7 +19,52 @@ const ChangeHistoryService = game.GetService("ChangeHistoryService");
 export class History {
 	private lastRecording: string | undefined;
 
-	constructor() {}
+	/**
+	 * Wrapper for `record()` that allows you to quickly try an operation and abort changes if it fails.
+	 *
+	 * @param config
+	 * @param fn
+	 * @returns value or error.
+	 */
+	public async try<T extends Callback>(
+		name: string,
+		fn: T,
+		...args: Parameters<T>
+	): Promise<ReturnType<T>>;
+	/**
+	 * Wrapper for `record()` that allows you to quickly try an operation and abort changes if it fails.
+	 *
+	 * Within the configuration, you may choose the action that is taken upon successful execution.
+	 * This will default to `commit`.
+	 *
+	 * @param config
+	 * @param fn
+	 * @returns value or error.
+	 */
+	public async try<T extends Callback>(
+		config: RecordConfig,
+		fn: T,
+		...args: Parameters<T>
+	): Promise<ReturnType<T>>;
+	public async try<T extends Callback>(
+		config: RecordConfig | string,
+		fn: T,
+		...args: Parameters<T>
+	): Promise<ReturnType<T>> {
+		if (typeIs(config, "string")) config = { name: config };
+		config.onSuccess ??= "commit";
+
+		const recording = this.record(config.name, config.description);
+		const [success, result] = pcall(fn, ...(args as unknown[]));
+
+		if (success) {
+			recording[config.onSuccess]();
+			return result;
+		}
+
+		recording.cancel();
+		throw result;
+	}
 
 	/**
 	 * API for recoding waypoints to undo/redo history.
